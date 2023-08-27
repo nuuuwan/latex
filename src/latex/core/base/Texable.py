e55ff644b25e71965a78b4c61c81abfc0e69683d
nuuuwan <nuuuwan@gmail.com>
2023-08-27 16:29:11 +0530

    \n\n

--- src/latex/core/base/Texable.py
+++ src/latex/core/base/Texable.py
@@ -19,7 +19,7 @@ class Texable:
 
     @property
     def tex(self) -> str:
-        return '\n'.join(self.tex_lines)
+        return '\n\n'.join(self.tex_lines)
 
     def write(self, path):
         File(path).write(self.tex)
