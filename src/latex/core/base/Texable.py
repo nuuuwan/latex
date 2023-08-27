4550ea50d1e43a82c7e7e178804ad6ffafede0c9
nuuuwan <nuuuwan@gmail.com>
2023-08-27 16:32:08 +0530

    lint

--- src/latex/core/base/Texable.py
+++ src/latex/core/base/Texable.py
@@ -20,9 +20,9 @@ class Texable:
     @property
     def tex(self) -> str:
         s = '\n\n'.join(self.tex_lines)
-        while '\n'*3 in s:
-            s = s.replace('\n'*3, '\n'*2)
+        while '\n' * 3 in s:
+            s = s.replace('\n' * 3, '\n' * 2)
         return s
-    
+
     def write(self, path):
         File(path).write(self.tex)
