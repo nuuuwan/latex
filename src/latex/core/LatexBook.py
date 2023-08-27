9dcaf1d14689f05220921d9313385cc67c50bc03
nuuuwan <nuuuwan@gmail.com>
2023-08-27 13:20:57 +0530

    Added author

--- src/latex/core/LatexBook.py
+++ src/latex/core/LatexBook.py
@@ -1,17 +1,17 @@
+from latex.core.base.Tag import Tag
 from latex.core.base.Texable import Texable
 from latex.core.native.Document import Document
 from latex.core.native.DocumentClass import DocumentClass
 from latex.core.native.Title import Title
-from latex.core.base.Tag import Tag
 
 
 class LatexBook(Texable):
-    def __init__(self, title, *children):
+    def __init__(self, title: str, author: str, *children):
         Texable.__init__(
             self,
             DocumentClass('book'),
             Document(
-                Title(title, 'Test Author', ' '),
+                Title(title, author, ' '),
                 Tag('tableofcontents'),
                 *children,
             ),
