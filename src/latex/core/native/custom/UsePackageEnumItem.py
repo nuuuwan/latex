215d51509de9c725e27cbe73c65f3d7d719507e2
nuuuwan <nuuuwan@gmail.com>
2023-08-28 08:12:26 +0530

    class UsePackageEnumItem

---
+++ src/latex/core/native/custom/UsePackageEnumItem.py
@@ -0,0 +1,22 @@
+from latex.core.base.Tag import Tag
+from latex.core.base.Texable import Texable
+from latex.core.native.UsePackage import UsePackage
+
+MAX_LIST_DEPTH = 10
+
+
+class UsePackageEnumItem(Texable):
+    def __init__(self):
+        children = [
+            UsePackage('enumitem'),
+            Tag('setlistdepth', MAX_LIST_DEPTH),
+        ]
+        for i in range(MAX_LIST_DEPTH):
+            children.append(
+                Tag(
+                    'setlist',
+                    'label=\\textbullet',
+                    f'itemize,{i+1}',
+                )
+            )
+        Texable.__init__(self, *children)
