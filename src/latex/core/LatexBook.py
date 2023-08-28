780b3774fc69f3b014c76360dc6c40699ac5b1ff
nuuuwan <nuuuwan@gmail.com>
2023-08-28 08:12:39 +0530

    class UsePackageEnumItem

--- src/latex/core/LatexBook.py
+++ src/latex/core/LatexBook.py
@@ -1,6 +1,7 @@
 from latex.core.base.Dict import Dict
 from latex.core.base.Tag import Tag
 from latex.core.base.Texable import Texable
+from latex.core.native.custom.UsePackageEnumItem import UsePackageEnumItem
 from latex.core.native.Document import Document
 from latex.core.native.DocumentClass import DocumentClass
 from latex.core.native.Title import Title
@@ -16,21 +17,7 @@ class LatexBook(Texable):
             UsePackage('xcolor'),
             UsePackage('dirtytalk'),
             UsePackage('hyperref'),
-            # UsePackage('libertine'),
-            # UsePackage('fontenc', 'T1'),
-            UsePackage('enumitem'),
-            Tag('setlistdepth', 10),
-            '\\renewlist{itemize}{itemize}{20}',
-            '\\setlist[itemize,1]{label=\\textbullet}',
-            '\\setlist[itemize,2]{label=\\textbullet}',
-            '\\setlist[itemize,3]{label=\\textbullet}',
-            '\\setlist[itemize,4]{label=\\textbullet}',
-            '\\setlist[itemize,5]{label=\\textbullet}',
-            '\\setlist[itemize,6]{label=\\textbullet}',
-            '\\setlist[itemize,7]{label=\\textbullet}',
-            '\\setlist[itemize,8]{label=\\textbullet}',
-            '\\setlist[itemize,9]{label=\\textbullet}',
-            '\\setlist[itemize,10]{label=\\textbullet}',
+            UsePackageEnumItem(),
             UsePackage(
                 'geometry',
                 Dict(
@@ -47,7 +34,7 @@ class LatexBook(Texable):
             Tag('pagestyle', 'fancy'),
             Tag('fancyhead', ' ', 'R'),
             Tag('fancyhead', ' ', 'L'),
-            '\\renewcommand{\\headrulewidth}{0pt}',
+            Tag('renewcommand', [Tag('headrulewidth'), '0pt']),
             Document(
                 Title(title, author, ' '),
                 Tag('tableofcontents'),
