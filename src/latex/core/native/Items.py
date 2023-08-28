a186f27a6e4d8ff0942fc5912932d3a22f506102
nuuuwan <nuuuwan@gmail.com>
2023-08-28 08:01:36 +0530

    Force only Texable items

--- src/latex/core/native/Items.py
+++ src/latex/core/native/Items.py
@@ -1,8 +1,15 @@
 from latex.core.base.BeginEnd import BeginEnd
 from latex.core.base.Tag import Tag
+from latex.core.base.Texable import Texable
 
 
 class Items(BeginEnd):
-    def __init__(self, *inner: list):
-        items = [Tag('item', item) for item in inner]
+    def __init__(self, *item_texable_list: list):
+        for item_texable in item_texable_list:
+            assert isinstance(item_texable, Texable)
+
+        items = [
+            Tag('item', item_texable.tex)
+            for item_texable in item_texable_list
+        ]
         BeginEnd.__init__(self, 'itemize', *items)
