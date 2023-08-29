dfe363c1379bebdb331c284912cf046ac9a534c6
nuuuwan <nuuuwan@gmail.com>
2023-08-29 08:59:38 +0530

    Added comment line

--- src/latex/core/base/DocumentSection.py
+++ src/latex/core/base/DocumentSection.py
@@ -1,7 +1,8 @@
+from latex.core.base.Comment import Comment
 from latex.core.base.Tag import Tag
 from latex.core.base.Texable import Texable
 
 
 class DocumentSection(Texable):
     def __init__(self, tag_name: str, name: str, *children):
-        Texable.__init__(self, Tag(tag_name, name), *children)
+        Texable.__init__(self, Comment.line(), Tag(tag_name, name), *children)
