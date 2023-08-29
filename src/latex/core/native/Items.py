2cfb4eab45b31ae39afabef544560120dbc6706d
nuuuwan <nuuuwan@gmail.com>
2023-08-29 15:40:54 +0530

    Empty items BUG

--- src/latex/core/native/Items.py
+++ src/latex/core/native/Items.py
@@ -7,9 +7,9 @@ class Items(BeginEnd):
     def __init__(self, *item_texable_list: list):
         for item_texable in item_texable_list:
             assert isinstance(item_texable, Texable)
+            assert len(item_texable.tex.strip()) > 0
         items = [
             Tag('item', item_texable.tex)
             for item_texable in item_texable_list
-            if item_texable.tex
         ]
         BeginEnd.__init__(self, 'itemize', *items)
